68a6d90a9a6789bd5fcbb4c4ea0e0df1487bd362
Crowdbotics <team@crowdbotics.com>
2021-08-11 22:47:15 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "old_river_29734Identifier",
+  url: "https://old_river_29734Identifier.botics.co",
+  api: "https://old_river_29734Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
